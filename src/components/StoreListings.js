1cf9e88cf5a005308993c8d31787fb3dfe6e7aa7
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-03-27 14:11:33 -0400

    Added Components Folder

---
+++ src/components/StoreListings.js
@@ -0,0 +1,14 @@
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+export default function StoreListings() {
+    return (
+        <div style={{ margin: "auto" }}>
+            
+        </div>
+    
+    )
+}
